04f4879bda052a31f16241d9bb1b33b8d973f3f5
Peter Verhas <peter@verhas.com>
2023-04-24 15:25:11 +0200

    this version uses less reallocation of string and Point structures

--- mandel.go
+++ mandel.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%s", Mandel(100))
+	fmt.Printf("%s", string(Mandel(1000000)))
 }
 
 type Point struct {
@@ -11,55 +11,60 @@ type Point struct {
 	y float64
 }
 
-func Mandel(max_iter int32) string {
-	width := 80
-	height := 40
-	x_min := -2.0
-	x_max := 1.0
-	y_min := -1.5
-	y_max := 1.5
-	x_step := (x_max - x_min) / float64(width)
-	y_step := (y_max - y_min) / float64(height)
+const width = 80
+const height = 40
 
-	var result = ""
+func Mandel(maxIter int32) []rune {
+	xMin := -2.0
+	xMax := 1.0
+	yMin := -1.5
+	yMax := 1.5
+	xStep := (xMax - xMin) / float64(width)
+	yStep := (yMax - yMin) / float64(height)
+
+	result := make([]rune, (width+1)*height)
 	var y float64
+	var x float64
+	c := &Point{0, 0}
+	i := 0
 	for y = 0; y < float64(height); y++ {
-		var x float64
 		for x = 0; x < float64(width); x++ {
-			c := &Point{x_min + x_step*x, y_min + y_step*y}
-			n := mandelbrot_set(c, max_iter)
-
-			var pixel string
-			switch {
-			case 0 <= n && n <= 10:
-				pixel = " "
-			case 11 <= n && n <= 20:
-				pixel = "."
-			case 21 <= n && n <= 30:
-				pixel = "+"
-			case 31 <= n && n <= 40:
-				pixel = "="
-			case 41 <= n && n <= 50:
-				pixel = "?"
-			case 51 <= n && n <= 60:
-				pixel = "#"
-			case 61 <= n && n <= 70:
-				pixel = ":"
-			default:
-				pixel = "*"
-			}
-			result = result + pixel
+			c.x, c.y = xMin+xStep*x, yMin+yStep*y
+			result[i] = pixel2rune(mandelbrotSet(c, maxIter))
+			i++
 		}
-		result += "\n"
+		result[i] = '\n'
+		i++
 	}
 	return result
 }
 
-func mandelbrot_set(c *Point, max_iter int32) int32 {
+func pixel2rune(n int32) rune {
+	switch {
+	case 0 <= n && n <= 10:
+		return ' '
+	case 11 <= n && n <= 20:
+		return '.'
+	case 21 <= n && n <= 30:
+		return '+'
+	case 31 <= n && n <= 40:
+		return '='
+	case 41 <= n && n <= 50:
+		return '?'
+	case 51 <= n && n <= 60:
+		return '#'
+	case 61 <= n && n <= 70:
+		return ':'
+	default:
+		return '*'
+	}
+}
+
+func mandelbrotSet(c *Point, maxIter int32) int32 {
 	z := &Point{0.0, 0.0}
 	var n int32 = 0
-	for z.x*z.x+z.y*z.y < 4.0 && n < max_iter {
-		z = &Point{z.x*z.x - z.y*z.y + c.x, 2.0*z.x*z.y + c.y}
+	for z.x*z.x+z.y*z.y < 4.0 && n < maxIter {
+		z.x, z.y = z.x*z.x-z.y*z.y+c.x, 2.0*z.x*z.y+c.y
 		n++
 	}
 	return n
